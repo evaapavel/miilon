08e66d861301b2590fccea4a0730df6b866777ee
Pavel Foltyn <pfoltyn@newps.cz>
2017-11-30 15:24:23 +0100

    TECH Miilon - Add the SPA (single page application) to the repo.

---
+++ Miilon/script.js
@@ -0,0 +1,256 @@
+//alert("Hi there, mate!");
+
+
+// All data available to this vocabulary exercise app.
+var vocabulary = {
+
+  "wordSets" : [
+  
+    {
+      "source": "ÉDITO B2",
+      "unit": "unité 2",
+      "page": 32,
+      "section": "DU CÔTÉ DES ANIMAUX",
+      "words": [
+        { "foreign": "aboyer", "grammar": "vi", "pronunciation": "abu^aje.", "meaning": "štěkat" },
+        { "foreign": "bêler", "grammar": "vi", "pronunciation": "bele.", "meaning": "bečet" },
+        { "foreign": "bourdonner", "grammar": "vi", "pronunciation": "burdone.", "meaning": "bzučet, hučet" },
+        { "foreign": "'hurler", "grammar": "vi", "pronunciation": "ürle.", "meaning": "výt, řvát" },
+        { "foreign": "chanter", "grammar": "vi vt", "pronunciation": "ša~te.", "meaning": "zpívat" },
+        { "foreign": "meugler", "grammar": "vi", "pronunciation": "mögle.", "meaning": "bučet" },
+        { "foreign": "miauler", "grammar": "vi", "pronunciation": "mjo.le.", "meaning": "mňoukat" },
+        { "foreign": "parler", "grammar": "vi", "pronunciation": "parle.", "meaning": "mluvit" },
+        { "foreign": "rugir", "grammar": "vi", "pronunciation": "rüži:r", "meaning": "řvát" },
+        { "foreign": "siffler", "grammar": "vi", "pronunciation": "sifle.", "meaning": "pískat, hvízdat, syčet, fičet, sípat" },
+      ]
+    },
+  
+    {
+      "source": "Papier",
+      "unit": "Serge Gainsbourg",
+      "page": 0,
+      "section": "Sois belle et tais-toi",
+      "words": [
+        { "foreign": "ramier", "grammar": "nm", "pronunciation": "ramje.", "meaning": "doupňák (holub)" },
+        { "foreign": "moineau", "grammar": "nm", "pronunciation": "mu^ano.", "meaning": "vrabec" },
+        { "foreign": "poule", "grammar": "nf", "pronunciation": "pul", "meaning": "slepice" },
+        { "foreign": "pie", "grammar": "nf", "pronunciation": "pi", "meaning": "straka" },
+        { "foreign": "chameau", "grammar": "nm", "pronunciation": "šamo.", "meaning": "velbloud (dvouhrbý)" },
+        { "foreign": "'hibou", "grammar": "nm", "pronunciation": "ibu", "meaning": "sova" },
+        { "foreign": "panthère", "grammar": "nf", "pronunciation": "pa~te:r", "meaning": "pardál, levhart" },
+        { "foreign": "grue", "grammar": "nf", "pronunciation": "grü", "meaning": "jeřáb (pták, stroj)" },
+        { "foreign": "éléphant", "grammar": "nm", "pronunciation": "e.le.fa~", "meaning": "slon" },
+        { "foreign": "jument", "grammar": "nf", "pronunciation": "žüma~", "meaning": "kobyla, klisna" },
+        { "foreign": "chouette", "grammar": "nf", "pronunciation": "šu^et", "meaning": "sova" },
+        { "foreign": "brebis", "grammar": "nf", "pronunciation": "br@bi", "meaning": "ovce" },
+        { "foreign": "crapaud", "grammar": "nm", "pronunciation": "krapo.", "meaning": "ropucha, žába" },
+        { "foreign": "poulet", "grammar": "nm", "pronunciation": "pule", "meaning": "kuře" },
+        { "foreign": "corbeau", "grammar": "nm", "pronunciation": "korbo.", "meaning": "krkavec, havran" },
+        { "foreign": "geai", "grammar": "nm", "pronunciation": "že", "meaning": "sojka" },
+        { "foreign": "cerf", "grammar": "nm", "pronunciation": "se:r", "meaning": "jelen" },
+        { "foreign": "âne", "grammar": "nm", "pronunciation": "a:n", "meaning": "osel" },
+        { "foreign": "lion", "grammar": "nm", "pronunciation": "ljo~", "meaning": "lev" },
+        { "foreign": "cane", "grammar": "nf", "pronunciation": "kan", "meaning": "kachna (samice)" },
+        { "foreign": "taureau", "grammar": "nm", "pronunciation": "toro.", "meaning": "býk" },
+        { "foreign": "dindon", "grammar": "nm", "pronunciation": "de~do~", "meaning": "krocan" },
+        { "foreign": "paon", "grammar": "nm", "pronunciation": "pa~", "meaning": "páv" },
+        { "foreign": "caille", "grammar": "nf", "pronunciation": "ka:j", "meaning": "křepelka" },
+        { "foreign": "serpent", "grammar": "nm", "pronunciation": "serpa~", "meaning": "had" },
+      ]
+    },
+  
+    {
+      "source": "ÉDITO B2",
+      "unit": "unité 3",
+      "page": 40,
+      "section": "LA COMMUNICATION",
+      "words": [
+        { "foreign": "communiqué de presse", "grammar": "nm", "pronunciation": "komünike. d@ pres", "meaning": "tiskové oznámení, tisková zpráva" },
+        { "foreign": "couverture médiatique", "grammar": "nf", "pronunciation": "kuvertü:r me.djatik", "meaning": "mediální zástěrka" },
+        { "foreign": "conférence de presse", "grammar": "nf", "pronunciation": "ko~fe.ra~:s d@ pres", "meaning": "tisková konference" },
+        { "foreign": "information", "grammar": "nf", "pronunciation": "e~formasjo~", "meaning": "zpráva, informace" },
+        { "foreign": "liberté d'information", "grammar": "nf", "pronunciation": "liberte. de~formasjo~", "meaning": "svoboda informací, svobodný přístup k informacím" },
+        { "foreign": "censure", "grammar": "nf", "pronunciation": "sa~sü:r", "meaning": "cenzura, kritika, výtka" },
+        { "foreign": "média", "grammar": "nm", "pronunciation": "me.dja", "meaning": "médium, sdělovací prostředek" },
+        { "foreign": "médiatisation", "grammar": "nf", "pronunciation": "me.djatizasjo~", "meaning": "medializace" },
+        { "foreign": "médiatique", "grammar": "adj", "pronunciation": "me.djatik", "meaning": "mediální" },
+        { "foreign": "multimédia", "grammar": "adj", "pronunciation": "mültime.dja", "meaning": "týkající se hromadných sdělovacích prostředků, masmediální, multimediální" },
+      ]
+    },
+  
+  ]
+  
+};
+
+
+
+
+// HTML table with vocabulary "sections".
+var sectionsTable = document.getElementById("tableSections");
+// Rows of the sections table.
+var sectionsTableRows = sectionsTable.children;
+
+// Words to be offerred to the user.
+var selectedWords = [];
+
+
+//for (var wordSet : vocabulary.wordSets) {
+//  alert(wordSet.section);
+//}
+for (var i = 0; i < vocabulary.wordSets.length; i++) {
+  var wordSet = vocabulary.wordSets[i];
+  //alert(wordSet.section);
+  //sectionsTable.children
+  //sectionsTableRows.
+  var newRow = sectionsTable.insertRow(i + 1);
+
+  var cellMark = newRow.insertCell(0);
+
+  var cellSource = newRow.insertCell(1);
+  var cellUnit = newRow.insertCell(2);
+  var cellPage = newRow.insertCell(3);
+  var cellSection = newRow.insertCell(4);
+
+  cellMark.innerHTML = "<input type='checkbox' id='checkboxMarkSection" + i + "' />";
+
+  cellSource.innerHTML = wordSet.source;
+  cellUnit.innerHTML = wordSet.unit;
+  cellPage.innerHTML = wordSet.page;
+  cellSection.innerHTML = wordSet.section;
+}
+
+
+
+// var wordSetIndex = 0;
+// var wordIndex = 0;
+
+// ***
+//alert(vocabulary.words[wordIndex].foreign);
+// ***
+
+// ***
+//$("#spanForeign").value = vocabulary.words[wordIndex].foreign;
+//$("#spanForeign").html = vocabulary.words[wordIndex].foreign;
+// ***
+
+
+// ***
+//alert(document.getElementById("spanForeign").text);
+//alert(document.getElementById("spanForeign").innerHTML);
+// ***
+
+// ***
+//$("#spanForeign").text(vocabulary.words[wordIndex].foreign);
+//document.getElementById("spanForeign").text = vocabulary.words[wordIndex].foreign;
+// ***
+//document.getElementById("spanForeign").innerHTML = vocabulary.words[wordIndex].foreign;
+//document.getElementById("spanGrammar").innerHTML = vocabulary.words[wordIndex].grammar;
+//document.getElementById("spanPronunciation").innerHTML = vocabulary.words[wordIndex].pronunciation;
+//document.getElementById("spanMeaning").innerHTML = vocabulary.words[wordIndex].meaning;
+// ***
+//document.getElementById("spanForeign").innerHTML = vocabulary.wordSets[0].words[wordIndex].foreign;
+//document.getElementById("spanGrammar").innerHTML = vocabulary.wordSets[0].words[wordIndex].grammar;
+//document.getElementById("spanPronunciation").innerHTML = vocabulary.wordSets[0].words[wordIndex].pronunciation;
+//document.getElementById("spanMeaning").innerHTML = vocabulary.wordSets[0].words[wordIndex].meaning;
+// ***
+//document.getElementById("spanForeign").innerHTML = vocabulary.wordSets[1].words[wordIndex].foreign;
+//document.getElementById("spanGrammar").innerHTML = vocabulary.wordSets[1].words[wordIndex].grammar;
+//document.getElementById("spanPronunciation").innerHTML = vocabulary.wordSets[1].words[wordIndex].pronunciation;
+//document.getElementById("spanMeaning").innerHTML = vocabulary.wordSets[1].words[wordIndex].meaning;
+// ***
+
+
+// document.getElementById("spanForeign").innerHTML = vocabulary.wordSets[wordSetIndex].words[wordIndex].foreign;
+// document.getElementById("spanGrammar").innerHTML = vocabulary.wordSets[wordSetIndex].words[wordIndex].grammar;
+// document.getElementById("spanPronunciation").innerHTML = vocabulary.wordSets[wordSetIndex].words[wordIndex].pronunciation;
+// document.getElementById("spanMeaning").innerHTML = vocabulary.wordSets[wordSetIndex].words[wordIndex].meaning;
+
+
+
+
+// ***
+// Gets a random integer value between given limits.
+// ***
+// Parameters:
+// minIntIncl ....................... Lower limit of generated integers (including).
+// maxIntIncl ....................... Upper limit of generated integers (including).
+// ***
+// Returns:
+// Returns a random integer that matches the above conditions (n, minIntIncl <= n <= maxIntIncl).
+// ***
+function getRandomIntFromInterval(minIntIncl, maxIntIncl) {
+  var n = Math.floor(Math.random() * (maxIntIncl - minIntIncl + 1)) + minIntIncl;
+  return n;
+}
+
+
+
+
+// ***
+// Handles the Click event on the button buttonRefresh.
+// ***
+// Parameters:
+// ***
+// Returns:
+// ***
+function buttonRefresh_onClick() {
+
+  selectedWords = [];
+  
+  //alert("Inside buttonRefresh_onClick...");
+  //alert("selectedWords.length = " + selectedWords.length);
+
+  //selectedWords = [];
+  var selectedWordCounter = 0;
+  
+  for (var i = 0; i < vocabulary.wordSets.length; i++) {
+    //var cellMark = sectionsTableRows[i].children[0];
+    var checkboxMark = document.getElementById("checkboxMarkSection" + i);
+    if (checkboxMark.checked) {
+      var currentSection = vocabulary.wordSets[i].words;
+      //for (var j = 0; j < vocabulary.wordSets[i].words.length
+      for (var j = 0; j < currentSection.length; j++) {
+        selectedWords[selectedWordCounter] = currentSection[j];
+        selectedWordCounter++;
+      }
+    }
+  }
+
+  //alert("selectedWords.length = " + selectedWords.length);
+
+}
+
+
+
+
+// ***
+// Handles the Click event on the button buttonNext.
+// ***
+// Parameters:
+// ***
+// Returns:
+// ***
+function buttonNext_onClick() {
+  
+  //alert("Inside buttonNext_onClick...");
+  //alert("selectedWords.length = " + selectedWords.length);
+
+  //var randomWordIndex = getRandomIntFromInterval(0, selectedWords.length);
+  var randomWordIndex = getRandomIntFromInterval(0, selectedWords.length - 1);
+
+  //alert("randomWordIndex = " + randomWordIndex);
+  
+  // document.getElementById("spanForeign").innerHTML = vocabulary.wordSets[wordSetIndex].words[wordIndex].foreign;
+  // document.getElementById("spanGrammar").innerHTML = vocabulary.wordSets[wordSetIndex].words[wordIndex].grammar;
+  // document.getElementById("spanPronunciation").innerHTML = vocabulary.wordSets[wordSetIndex].words[wordIndex].pronunciation;
+  // document.getElementById("spanMeaning").innerHTML = vocabulary.wordSets[wordSetIndex].words[wordIndex].meaning;
+
+  document.getElementById("spanForeign").innerHTML = selectedWords[randomWordIndex].foreign;
+  document.getElementById("spanGrammar").innerHTML = selectedWords[randomWordIndex].grammar;
+  document.getElementById("spanPronunciation").innerHTML = selectedWords[randomWordIndex].pronunciation;
+  document.getElementById("spanMeaning").innerHTML = selectedWords[randomWordIndex].meaning;
+  
+}
+  
+
+
