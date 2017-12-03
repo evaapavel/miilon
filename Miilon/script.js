925907f9036e57e8bef802629d287d87cb3724a7
Pavel Foltyn <pfoltyn@newps.cz>
2017-12-03 05:22:45 +0100

    CHANGE Miilon - Add a timer example.

--- Miilon/script.js
+++ Miilon/script.js
@@ -276,6 +276,47 @@ function buttonNext_onClick() {
   document.getElementById("spanMeaning").innerHTML = selectedWords[randomWordIndex].meaning;
   
 }
+
+
+
+
+function startTimer(duration, display) {
+
+  var timer = duration, minutes, seconds;
+
+  setInterval(function() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    // ***
+    if (timer % 4 == 0) {
+      buttonNext_onClick();
+    }
+    // ***
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+
+}
+
+
+
+
+function buttonTimer_onClick() {
+  var fiveMinutes = 60 * 5;
+  var display = document.querySelector('#spanTime');
+  startTimer(fiveMinutes, display);
+}
+
+
+
   
 
 
